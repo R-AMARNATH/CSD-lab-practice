d430637a57c92df85bb3ab720c19a0971c345e5f
Amarnath <amarcs062002@gmail.com>
2025-10-19 11:57:27 +0100

    added test project

---
+++ bmi2025lab.Teats/Test1.cs
@@ -0,0 +1,27 @@
+﻿using BMICalculator;
+
+namespace bmi2025lab.Teats
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void BMICategory_ShouldReturnNormal_ForHealthyBMI()
+        {
+            // Arrange
+            var bmi = new BMI
+            {
+                WeightStones = 11,   // ~70 kg
+                WeightPounds = 0,
+                HeightFeet = 5,
+                HeightInches = 8
+            };
+
+            // Act
+            var category = bmi.BMICategory;
+
+            // Assert
+            Assert.AreEqual(BMICategory.Normal, category, "BMI category should be Normal");
+        }
+    }
+}
